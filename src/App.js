1cbc9364f3a320841a3eb082b12e353af709fbff
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-13 12:08:08 +0330

    wired router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 
 const App = () => (
-  <>
-    <Navbar />
-    <Landing />
-  </>
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </>
+  </Router>
 );
 
 export default App;
