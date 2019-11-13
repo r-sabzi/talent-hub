bb3ac953355c17d3e18251e676985fd158cc55a2
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-13 17:42:48 +0330

    added redux boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./App.css";
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './store'
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 
 const App = () => (
-  <Router>
-    <>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </>
+    </Router>
+  </Provider>
 );
 
 export default App;
