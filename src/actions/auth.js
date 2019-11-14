c8b15b942a4d6466f35ab1a8c2766bf26c5c15ee
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-14 11:55:30 +0330

    auth reducer and action

---
+++ src/actions/auth.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import { setAlert } from "./alert";
+
+
+export const register = ({ name, email, password }) => async dispatch => {
+  const config = {
+    headers: {
+      "content-type": "application/json"
+    }
+  };
+
+  const body = JSON.stringify({ name, email, password });
+
+  try {
+    const res = await axios.post("api/users", body, config);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+    console.log(errors);
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
+};
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
-import PropTypes from 'prop-types'
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-
-const Register = ({setAlert}) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,7 +21,7 @@ const Register = ({setAlert}) => {
     if (password !== password2) {
       setAlert("password do not match", "danger");
     } else {
-      console.log("success");
+      register({name,email,password})
     }
   };
 
@@ -85,7 +85,8 @@ const Register = ({setAlert}) => {
 };
 
 Register.prototype = {
-  setAlert:PropTypes.func.isRequired
-}
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
+};
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
