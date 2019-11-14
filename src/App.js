ad680ab6cdbb6a411c0c50496989dd3e02e8aebe
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-14 13:13:51 +0330

    added token checking and user data fetching  action and reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -8,23 +8,34 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 import Alert from "./components/layout/Alert";
+import {loadUser} from './actions/auth';
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert/>
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser)
+  },[])
+  return (
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
+  );
+}
 
 export default App;
