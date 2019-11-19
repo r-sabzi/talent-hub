950d13611ec62165f8cb0640813becd1b4d93478
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-19 17:55:58 +0330

    added ProfileTop and profileAbout components

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
 import { connect } from "react-redux";
 import { getProfileById } from "../../actions/profile";
-
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 const Profile = ({
   getProfileById,
   profile: { profile, loading },
@@ -13,7 +14,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
   return (
     <>
       {profile === null || loading ? (
@@ -30,6 +31,10 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+          <div className="profil-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
         </>
       )}
     </>
