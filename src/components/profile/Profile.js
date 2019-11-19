82f6d4481b762eb856da2fa218a51a2f9b555b1c
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-19 13:24:09 +0330

    added Profile component

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import { connect } from "react-redux";
+import { getProfileById } from "../../actions/profile";
+
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById]);
+  return (
+    <>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to="/profile" className="btn btn-light">
+            Back To Profile
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </>
+      )}
+    </>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+export default connect(mapStateToProps, { getProfileById })(Profile);
