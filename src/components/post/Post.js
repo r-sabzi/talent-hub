59fc4df13c3ce5fd24455265a6de27aac790fcd5
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-20 19:18:56 +0330

    Added Post component

---
+++ src/components/post/Post.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getPost } from "../../actions/post";
+import Spinner from "../layout/Spinner";
+import PostItem from "../posts/PostItem";
+import { Link } from "react-router-dom";
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.param.id);
+  }, [getPost]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <Link to="/posts" className="btn">
+        Back to Posts
+      </Link>
+      <PostItem post={post} showAction={false} />
+    </>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Moment from "moment";
-import { addLike, removeLike,deletePost } from "../../actions/post";
+import { addLike, removeLike, deletePost } from "../../actions/post";
 
 const PostItem = ({
   auth,
   addLike,
   removeLike,
   deletePost,
+  showActions,
   post: { _id, text, name, avatar, user, likes, comments, date }
 }) => {
   return (
@@ -25,35 +26,48 @@ const PostItem = ({
         <p class="post-date">
           Posted on <Moment format="YYY/MM/DD">{date}</Moment>
         </p>
-        <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-up"></i>{" "}
-          {likes.length > 0 && <span>{likes.length}</span>}
-        </button>
-        <button
-          onClick={e => removeLike(_id)}
-          type="button"
-          class="btn btn-light"
-        >
-          <i class="fas fa-thumbs-down"></i>
-        </button>
-        <Link to={`/post/${_id}`} class="btn btn-primary">
-          Discussion{" "}
-          {comments.length > 0 && (
-            <span class="comment-count">{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && user === auth.user._id && (
-          <button
-            onClick={e => deletePost(_id)}
-            type="button"
-            class="btn btn-danger"
-          >
-            <i class="fas fa-times"></i>
-          </button>
+
+        {showActions && (
+          <>
+            <button
+              onClick={e => addLike(_id)}
+              type="button"
+              class="btn btn-light"
+            >
+              <i class="fas fa-thumbs-up"></i>{" "}
+              {likes.length > 0 && <span>{likes.length}</span>}
+            </button>
+            <button
+              onClick={e => removeLike(_id)}
+              type="button"
+              class="btn btn-light"
+            >
+              <i class="fas fa-thumbs-down"></i>
+            </button>
+            <Link to={`/posts/${_id}`} class="btn btn-primary">
+              Discussion{" "}
+              {comments.length > 0 && (
+                <span class="comment-count">{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+              <button
+                onClick={e => deletePost(_id)}
+                type="button"
+                class="btn btn-danger"
+              >
+                <i class="fas fa-times"></i>
+              </button>
+            )}
+          </>
         )}
       </div>
     </div>
   );
+};
+
+PostItem.defaultProp = {
+  showActions: true
 };
 
 PostItem.propTypes = {
@@ -67,4 +81,6 @@ PostItem.propTypes = {
 const mapStateToProps = state => ({
   auth: state.auth
 });
-export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
