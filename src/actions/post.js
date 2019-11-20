da45e95694731d2ea6032861a630cac9ef0463d6
r.sabzi <r.kh.sabzi@gmail.com>
2019-11-20 17:38:02 +0330

    Added add post functionality

--- src/actions/post.js
+++ src/actions/post.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
   GET_POSTS,
-  POST_ERROR,  
+  POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from "../actions/types";
 
 export const getPosts = () => async dispatch => {
@@ -61,7 +62,30 @@ export const deletePost = id => async dispatch => {
       payload: id
     });
 
-    dispatch(setAlert('Post Removed','success'))
+    dispatch(setAlert("Post Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  try {
+    const res = await axios.post(`/api/posts`, formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Post Created", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
